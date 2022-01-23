a7a56433ce919337c55359012d04d2e234d2328c
LouiseWats <81812239+LouiseWats@users.noreply.github.com>
2022-01-23 02:03:53 +0100

    Add project files.

---
+++ TextToSpeech/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace TextToSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.speechBox = new System.Windows.Forms.RichTextBox();
+            this.speech = new System.Windows.Forms.Button();
+            this.pause = new System.Windows.Forms.Button();
+            this.resume = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // speechBox
+            // 
+            this.speechBox.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.speechBox.BackColor = System.Drawing.Color.AliceBlue;
+            this.speechBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.speechBox.Location = new System.Drawing.Point(33, 178);
+            this.speechBox.Name = "speechBox";
+            this.speechBox.Size = new System.Drawing.Size(730, 241);
+            this.speechBox.TabIndex = 0;
+            this.speechBox.Text = "";
+            // 
+            // speech
+            // 
+            this.speech.BackColor = System.Drawing.Color.AliceBlue;
+            this.speech.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.speech.Location = new System.Drawing.Point(33, 120);
+            this.speech.Name = "speech";
+            this.speech.Size = new System.Drawing.Size(133, 52);
+            this.speech.TabIndex = 1;
+            this.speech.Text = "Speech";
+            this.speech.UseVisualStyleBackColor = false;
+            this.speech.Click += new System.EventHandler(this.speech_Click);
+            this.speech.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            // 
+            // pause
+            // 
+            this.pause.BackColor = System.Drawing.Color.AliceBlue;
+            this.pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pause.Location = new System.Drawing.Point(182, 120);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(133, 52);
+            this.pause.TabIndex = 1;
+            this.pause.Text = "Pause";
+            this.pause.UseVisualStyleBackColor = false;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            this.pause.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            // 
+            // resume
+            // 
+            this.resume.BackColor = System.Drawing.Color.AliceBlue;
+            this.resume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resume.Location = new System.Drawing.Point(330, 120);
+            this.resume.Name = "resume";
+            this.resume.Size = new System.Drawing.Size(133, 52);
+            this.resume.TabIndex = 1;
+            this.resume.Text = "Resume";
+            this.resume.UseVisualStyleBackColor = false;
+            this.resume.Click += new System.EventHandler(this.resume_Click);
+            this.resume.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(211, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 55);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Text to Speech";
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.AliceBlue;
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.Location = new System.Drawing.Point(630, 120);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(133, 52);
+            this.Exit.TabIndex = 1;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            this.Exit.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.Color.AliceBlue;
+            this.Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clear.Location = new System.Drawing.Point(479, 120);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(133, 52);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            this.Clear.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TextToSpeech.Properties.Resources.spacebackground;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.resume);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.speech);
+            this.Controls.Add(this.speechBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_mousedown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_mousemove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_mouseup);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox speechBox;
+        private System.Windows.Forms.Button speech;
+        private System.Windows.Forms.Button pause;
+        private System.Windows.Forms.Button resume;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ TextToSpeech/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Drawing.Drawing2D;
+
+namespace TextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        private bool dragging = false;
+        private Point dragCursor;
+        private Point dragForm;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initial text
+            speechBox.Text = ("A supernova is the biggest explosion that humans have ever seen. Each blast is the extremely bright, super-powerful explosion of a star. \n" +
+                "What causes a supernova? \n" +
+                "One type of supernova is caused by the “last hurrah” of a dying massive star.This happens when a star at least five times the mass of our sun goes out with a fantastic bang! \n" +
+                "Massive stars burn huge amounts of nuclear fuel at their cores, or centers.This produces tons of energy, so the center gets very hot. Heat generates pressure, and the pressure created by a star’s nuclear burning also keeps that star from collapsing. \n" +
+                "A star is in balance between two opposite forces.The star’s gravity tries to squeeze the star into the smallest, tightest ball possible. But the nuclear fuel burning in the star’s core creates strong outward pressure. This outward push resists the inward squeeze of gravity.");
+        }
+
+        // Text to speech button
+        private async void speech_Click(object sender, EventArgs e)
+        {
+            if (speechBox.Text != "")
+            {
+                synthesizer = new SpeechSynthesizer();
+                synthesizer.SpeakAsync(speechBox.Text);
+            }
+            else
+            {
+                speechBox.Text = ("You have not written anything");
+                synthesizer = new SpeechSynthesizer();
+                synthesizer.SpeakAsync(speechBox.Text);
+
+                await Task.Delay(2000);
+                speechBox.Clear();
+                speechBox.Focus();
+            }
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            if (synthesizer.State == SynthesizerState.Speaking)
+            {
+                synthesizer.Pause();
+            }
+        }
+
+        private void resume_Click(object sender, EventArgs e)
+        {
+            if (speechBox.Text == "")
+            {
+                synthesizer = new SpeechSynthesizer();
+                synthesizer.SpeakAsync(speechBox.Text);
+            } 
+            else if ((synthesizer.State == SynthesizerState.Paused) && (speechBox.Text != ""))
+            {
+                synthesizer.Resume();
+            }   
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dialogResult = MessageBox.Show("Do you want to exit the application?", "Warning", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                if (speechBox.Text != "")
+                {
+                    synthesizer.Pause();
+                }
+            }
+        }
+
+        /*************************************************************************************/
+
+        // Make form window movable
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            speechBox.Clear();
+            synthesizer.Dispose();
+        }
+
+        private void Form1_mousedown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            dragCursor = Cursor.Position;
+            dragForm = this.Location;
+        }
+
+        private void Form1_mousemove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursor));
+                this.Location = Point.Add(dragForm, new Size(dif));
+            }
+        }
+
+        private void Form1_mouseup(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        /*************************************************************************************/
+
+
+        // Make rounded corners for buttons and form
+        private GraphicsPath GetRoundPath(RectangleF rect, int radius)
+        {
+            float r2 = radius / 2f;
+            GraphicsPath GraphPath = new GraphicsPath();
+            GraphPath.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            GraphPath.AddLine(rect.X + r2, rect.Y, rect.Width - r2, rect.Y);
+            GraphPath.AddArc(rect.X + rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            GraphPath.AddLine(rect.Width, rect.Y + r2, rect.Width, rect.Height - r2);
+            GraphPath.AddArc(rect.X + rect.Width - radius,
+                             rect.Y + rect.Height - radius, radius, radius, 0, 90);
+            GraphPath.AddLine(rect.Width - r2, rect.Height, rect.X + r2, rect.Height);
+            GraphPath.AddArc(rect.X, rect.Y + rect.Height - radius, radius, radius, 90, 90);
+            GraphPath.AddLine(rect.X, rect.Height - r2, rect.X, rect.Y + r2);
+            GraphPath.CloseFigure();
+            return GraphPath;
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            RectangleF Rect = new RectangleF(0, 0, this.Width, this.Height);
+            using (GraphicsPath GraphPath = GetRoundPath(Rect, 25))
+            {
+                this.Region = new Region(GraphPath);
+                using (Pen pen = new Pen(Color.White, 1.50f))
+                {
+                    pen.Alignment = PenAlignment.Inset;
+                    e.Graphics.DrawPath(pen, GraphPath);
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+    
+
+
+
+
+
+
